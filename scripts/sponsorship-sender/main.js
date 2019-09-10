8d2f40281f000a0d9639d88b9c72943c8ae47996
Kento Nishi <kento24gs@outlook.com>
2019-09-09 23:48:19 -0700

    Added bulk sender

--- scripts/sponsorship-sender/main.js
+++ scripts/sponsorship-sender/main.js
@@ -7,21 +7,27 @@ const mg = mailgun({
 	apiKey: process.env.MailgunApiKey, 
 	domain: "vikehacks.tech" 
 });
-const variables = {
-	recipient_first_name: jsonConfiguration.recipient_first_name,
-	your_first_name: jsonConfiguration.your_first_name,
-	your_role: jsonConfiguration.your_role,
-	hackathon_month: jsonConfiguration.hackathon_month,
-	reasons_paragraph: jsonConfiguration.reasons_paragraph,
-	your_full_name: jsonConfiguration.your_full_name
-}
-const data = {
-	from: `${jsonConfiguration.your_full_name} <${jsonConfiguration.your_vikehacks_email}>`,
-	to: jsonConfiguration.recipient_email,
-	subject: "Hackathon Sponsorship",
-	template: "sponsor_email",
-	'h:X-Mailgun-Variables': JSON.stringify(variables)
-};
-mg.messages().send(data, function (error, body) {
-	console.log(body);
+
+var promises=[];
+jsonConfiguration.emails.forEach(emailConfiguration=>{
+	const variables = {
+		your_first_name: jsonConfiguration.your_first_name,
+		your_role: jsonConfiguration.your_role,
+		hackathon_month: jsonConfiguration.hackathon_month,
+		reasons_paragraph: emailConfiguration.reasons_paragraph,
+		recipient_first_name: emailConfiguration.recipient_first_name,
+		your_full_name: jsonConfiguration.your_full_name
+	}
+	const data = {
+		from: `${jsonConfiguration.your_full_name} <${jsonConfiguration.your_vikehacks_email}>`,
+		to: emailConfiguration.recipient_email,
+		subject: "Hackathon Sponsorship",
+		template: "sponsor_email",
+		'h:X-Mailgun-Variables': JSON.stringify(variables)
+	};
+	promises.push(mg.messages().send(data));
+});
+
+Promise.all(promises).then(()=>{
+	console.log("Finished sending all emails");
 });
