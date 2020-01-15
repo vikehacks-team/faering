893afc8af385ac9d939e0a7c5866e1cf59d15fab
Kento Nishi <kento24gs@outlook.com>
2020-01-15 00:00:45 -0800

    Sending out emails

--- scripts/sponsorship-sender/main.js
+++ scripts/sponsorship-sender/main.js
@@ -3,20 +3,21 @@ const DOMAIN = "vikehacks.tech";
 var fs = require('fs');
 
 var jsonConfiguration = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const mg = mailgun({ 
-	apiKey: process.env.MailgunApiKey, 
-	domain: "vikehacks.tech" 
+const mg = mailgun({
+	apiKey: process.env.MailgunApiKey,
+	domain: "vikehacks.tech"
 });
 
-var promises=[];
-jsonConfiguration.emails.forEach(emailConfiguration=>{
+var promises = [];
+jsonConfiguration.emails.forEach(emailConfiguration => {
 	const variables = {
 		your_first_name: jsonConfiguration.your_first_name,
 		your_role: jsonConfiguration.your_role,
 		hackathon_month: jsonConfiguration.hackathon_month,
 		reasons_paragraph: emailConfiguration.reasons_paragraph,
 		recipient_first_name: emailConfiguration.recipient_first_name,
-		your_full_name: jsonConfiguration.your_full_name
+		your_full_name: jsonConfiguration.your_full_name,
+		date: jsonConfiguration.date
 	}
 	const data = {
 		from: `${jsonConfiguration.your_full_name} <${jsonConfiguration.your_vikehacks_email}>`,
@@ -28,6 +29,6 @@ jsonConfiguration.emails.forEach(emailConfiguration=>{
 	promises.push(mg.messages().send(data));
 });
 
-Promise.all(promises).then(()=>{
+Promise.all(promises).then(() => {
 	console.log("Finished sending all emails");
 });
